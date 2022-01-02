c35030ff4bb41acda2015d23cda4a40851c7a718
Ian McCracken <ian.mccracken@gmail.com>
2022-01-02 15:17:58 -0600

    remove dumb func

--- encode.go
+++ encode.go
@@ -3,8 +3,6 @@ package bingo
 import (
 	"fmt"
 	"time"
-
-	"github.com/goccy/go-reflect"
 )
 
 func EncodeValue(b []byte, v interface{}, inverse bool) int {
@@ -32,36 +30,5 @@ func EncodeValue(b []byte, v interface{}, inverse bool) int {
 		return EncodeTime(b, c, inverse)
 	default:
 		panic(fmt.Sprintf("unknown type: %T", v))
-	}
-}
-
-func BEncodeValue(b []byte, v interface{}, inverse bool) int {
-	if v == nil {
-		return EncodeNil(b, inverse)
-	}
-	typ := reflect.TypeOf(v)
-	switch typ.Kind() {
-	case reflect.Int8:
-		return EncodeInt8(b, v.(int8), inverse)
-	case reflect.Int16:
-		return EncodeInt16(b, v.(int16), inverse)
-	case reflect.Int:
-		return EncodeInt32(b, int32(v.(int)), inverse)
-	case reflect.Int32:
-		return EncodeInt32(b, v.(int32), inverse)
-	case reflect.Int64:
-		return EncodeInt64(b, v.(int64), inverse)
-	case reflect.Float32:
-		return EncodeFloat32(b, v.(float32), inverse)
-	case reflect.Float64:
-		return EncodeFloat64(b, v.(float64), inverse)
-	case reflect.String:
-		return EncodeString(b, v.(string), inverse)
-	default:
-		if _, ok := v.(time.Time); ok {
-			return EncodeTime(b, v.(time.Time), inverse)
-		} else {
-			panic("jfkdls")
-		}
 	}
 }
