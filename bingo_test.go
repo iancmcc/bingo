010f7d04daa788f24839668c34dabf70d7732539
Ian McCracken <ian.mccracken@gmail.com>
2022-01-23 18:14:57 -0600

    Remove ginkgo

--- bingo_test.go
+++ bingo_test.go
@@ -2,123 +2,121 @@ package bingo_test
 
 import (
 	"bytes"
+	"errors"
 	"testing"
 
 	. "github.com/iancmcc/bingo"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-var _ = Describe("Packing", func() {
-
-	It("should pack values while preserving order", func() {
-		a, err := Pack(1, "hi", int64(67))
-		Ω(err).ShouldNot(HaveOccurred())
-		b, err := Pack(1, "hi 1", int64(67))
-		Ω(err).ShouldNot(HaveOccurred())
-		Ω(bytes.Compare(a, b)).Should(Equal(-1))
-	})
-
-	It("should pack values into a given array while preserving order", func() {
-		buf := make([]byte, 32)
-		bufd := make([]byte, 32)
-		PackTo(buf, 1, "hi", int64(67))
-		PackTo(bufd, 1, "hi 1", int64(67))
-		Ω(bytes.Compare(buf, bufd)).Should(Equal(-1))
-	})
-
-	It("should pack mixed-order values while preserving order", func() {
-		s := WithDesc(false, true, false)
-		a, err := s.Pack(1, "hi", int64(67))
-		Ω(err).ShouldNot(HaveOccurred())
-		b, err := s.Pack(1, "hi 1", int64(67))
-		Ω(err).ShouldNot(HaveOccurred())
-		Ω(bytes.Compare(a, b)).Should(Equal(1))
-	})
-
-	It("should pack mixed-order values into a given byte array while preserving order", func() {
-		buf := make([]byte, 32)
-		bufd := make([]byte, 32)
-		s := WithDesc(false, true, false)
-		s.PackTo(buf, 1, "hi", int64(67))
-		s.PackTo(bufd, 1, "hi 1", int64(67))
-		Ω(bytes.Compare(buf, bufd)).Should(Equal(1))
-	})
-})
-var _ = Describe("Unpacking", func() {
-
-	It("should unpack values", func() {
-		a := "this is a test"
-		b := int8(69)
-		c := float32(1.61803398875)
-		packed, err := Pack(a, b, c)
-		Ω(err).ShouldNot(HaveOccurred())
-
-		var (
-			adest string
-			bdest int8
-			cdest float32
-		)
-		Unpack(packed, &adest, &bdest, &cdest)
-		Ω(a).Should(Equal(adest))
-		Ω(b).Should(Equal(bdest))
-		Ω(c).Should(Equal(cdest))
-	})
-
-	It("should unpack only those asked for", func() {
-		a := "this is a test"
-		b := int8(69)
-		c := float32(1.61803398875)
-		packed, err := Pack(a, b, c)
-		Ω(err).ShouldNot(HaveOccurred())
-
-		var (
-			adest string
-			cdest float32
-		)
-		Unpack(packed, &adest, nil, &cdest)
-		Ω(a).Should(Equal(adest))
-		Ω(c).Should(Equal(cdest))
-	})
-
-	It("should unpack a value at a specific index", func() {
-		a := "this is a test"
-		b := int8(69)
-		c := float32(1.61803398875)
-		packed, err := Pack(a, b, c)
-		Ω(err).ShouldNot(HaveOccurred())
-
-		var (
-			adest string
-			cdest float32
-		)
-
-		UnpackIndex(packed, 2, &cdest)
-		Ω(c).Should(Equal(cdest))
-
-		UnpackIndex(packed, 0, &adest)
-		Ω(a).Should(Equal(adest))
-	})
-
-	It("should error when a nonexistent index is requested", func() {
-		a := "this is a test"
-		b := int8(69)
-		c := float32(1.61803398875)
-		packed, err := Pack(a, b, c)
-		Ω(err).ShouldNot(HaveOccurred())
-
-		var cdest float32
-		Ω(UnpackIndex(packed, 7, &cdest)).Should(HaveOccurred())
-	})
-
-	It("should error when unpacking a random string", func() {
-		b := []byte("abcde")
-		var adest string
-		Ω(Unpack(b, &adest)).Should(HaveOccurred())
-	})
-})
-
 func TestBingo(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Bingo Suite")
+
+	Convey("Bingo", t, func() {
+
+		Convey("should pack values while preserving order", func() {
+			a, err := Pack(1, "hi", int64(67))
+			So(err, ShouldBeNil)
+			b, err := Pack(1, "hi 1", int64(67))
+			So(err, ShouldBeNil)
+			So(bytes.Compare(a, b), ShouldEqual, -1)
+		})
+
+		Convey("should pack values into a given array while preserving order", func() {
+			buf := make([]byte, 32)
+			bufd := make([]byte, 32)
+			PackTo(buf, 1, "hi", int64(67))
+			PackTo(bufd, 1, "hi 1", int64(67))
+			So(bytes.Compare(buf, bufd), ShouldEqual, -1)
+		})
+
+		Convey("should pack mixed-order values while preserving order", func() {
+			s := WithDesc(false, true, false)
+			a, err := s.Pack(1, "hi", int64(67))
+			So(err, ShouldBeNil)
+			b, err := s.Pack(1, "hi 1", int64(67))
+			So(err, ShouldBeNil)
+			So(bytes.Compare(a, b), ShouldEqual, 1)
+		})
+
+		Convey("should pack mixed-order values into a given byte array while preserving order", func() {
+			buf := make([]byte, 32)
+			bufd := make([]byte, 32)
+			s := WithDesc(false, true, false)
+			s.PackTo(buf, 1, "hi", int64(67))
+			s.PackTo(bufd, 1, "hi 1", int64(67))
+			So(bytes.Compare(buf, bufd), ShouldEqual, 1)
+		})
+
+		Convey("should unpack values", func() {
+			a := "this is a test"
+			b := int8(69)
+			c := float32(1.61803398875)
+			packed, err := Pack(a, b, c)
+			So(err, ShouldBeNil)
+
+			var (
+				adest string
+				bdest int8
+				cdest float32
+			)
+			Unpack(packed, &adest, &bdest, &cdest)
+			So(a, ShouldEqual, adest)
+			So(b, ShouldEqual, bdest)
+			So(c, ShouldEqual, cdest)
+		})
+
+		Convey("should unpack only those asked for", func() {
+			a := "this is a test"
+			b := int8(69)
+			c := float32(1.61803398875)
+			packed, err := Pack(a, b, c)
+			So(err, ShouldBeNil)
+
+			var (
+				adest string
+				cdest float32
+			)
+			Unpack(packed, &adest, nil, &cdest)
+			So(a, ShouldEqual, adest)
+			So(c, ShouldEqual, cdest)
+		})
+
+		Convey("should unpack a value at a specific index", func() {
+			a := "this is a test"
+			b := int8(69)
+			c := float32(1.61803398875)
+			packed, err := Pack(a, b, c)
+			So(err, ShouldBeNil)
+
+			var (
+				adest string
+				cdest float32
+			)
+
+			UnpackIndex(packed, 2, &cdest)
+			So(c, ShouldEqual, cdest)
+
+			UnpackIndex(packed, 0, &adest)
+			So(a, ShouldEqual, adest)
+		})
+
+		Convey("should error when a nonexistent index is requested", func() {
+			a := "this is a test"
+			b := int8(69)
+			c := float32(1.61803398875)
+			packed, err := Pack(a, b, c)
+			So(err, ShouldBeNil)
+
+			var cdest float32
+			So(UnpackIndex(packed, 7, &cdest), ShouldResemble, errors.New("No data at index 7"))
+		})
+
+		Convey("should error when unpacking a random string", func() {
+			b := []byte("abcde")
+			var adest string
+			So(Unpack(b, &adest), ShouldResemble, errors.New("unknown type"))
+		})
+
+	})
+
 }
--- go.mod
+++ go.mod
@@ -4,20 +4,11 @@ go 1.17
 
 require (
 	github.com/goccy/go-reflect v1.1.0
-	github.com/onsi/ginkgo v1.16.4
-	github.com/onsi/gomega v1.14.0
 	github.com/smartystreets/goconvey v1.7.2
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 )
 
 require (
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
-	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/smartystreets/assertions v1.2.0 // indirect
-	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
